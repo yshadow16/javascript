efbf8ad489946f4d59d76d5f076fd73b64afbfa1
Davi Goulart Bastos <131214827+yshadow16@users.noreply.github.com>
2025-08-14 23:31:08 -0300

    exercícios 008, 009, 010 e exercício/desafio 011

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples!
+    console.log(`Você passou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
