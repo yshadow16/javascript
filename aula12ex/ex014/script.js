1b6aa59cecb2c512fd17ad4af53776c0a7a4fd92
Davi Goulart Bastos <131214827+yshadow16@users.noreply.github.com>
2025-08-20 20:17:51 -0300

    aula 12 de exercícios (modelo de site, ex014 e 015)

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -5,15 +5,15 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        // Bom dia!
+        // Bom Dia!
         img.src = "manha.png"
         document.body.style.background = "#e2cd9f"
     } else if (hora >= 12 && hora < 18) {
-        // Boa tarde!
+        // Boa Tarde!
         img.src = "tarde.png"
         document.body.style.background = "#b9846f"
     } else {
-        // Boa noite!
+        // Boa Noite!
         img.src = "noite.png"
         document.body.style.background = "#515154"
     }
